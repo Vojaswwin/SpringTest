98c414435e798316cc8dbc2da3f2fed2b749fe17
Vojaswwin <vojaswwin.ap@thoughtworks.com>
2022-10-20 18:24:55 +0530

    initial commit

---
+++ src/main/java/com/vojTests/springFrameworkTesting/SpringFrameworkTestingApplication.java
@@ -0,0 +1,13 @@
+package com.vojTests.springFrameworkTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vojTests/springFrameworkTesting/SpringFrameworkTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vojTests.springFrameworkTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
